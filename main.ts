7f82b79d356dcda75837971464eba55758a3779a
Luis Dourado <lfaddourado@gmail.com>
2025-08-09 00:08:21 -0300

    feat: add entrypoint

---
+++ main.ts
@@ -0,0 +1,108 @@
+import { Plugin } from "obsidian";
+import { TeamDocsSettings, DEFAULT_SETTINGS } from "./src/types/Settings";
+import { EditReservationManager } from "./src/managers/EditReservationManager";
+import { StatusIndicator } from "./src/ui/StatusIndicator";
+import { GitService } from "./src/services/GitService";
+import { FileHandler } from "./src/handlers/FileHandlers";
+import { CommandManager } from "./src/managers/CommandManager";
+import { UIManager } from "./src/managers/UIManager";
+import { TeamDocsSettingTab } from "./src/ui/SettingsTab";
+import {
+	ACTIVITY_FEED_VIEW,
+	TeamActivityFeedView,
+} from "./src/ui/TeamActivityFeed";
+
+/**
+ * Main plugin class for Team Docs Git Sync functionality
+ */
+export default class TeamDocsPlugin extends Plugin {
+	settings: TeamDocsSettings;
+	reservationManager: EditReservationManager;
+	statusIndicator: StatusIndicator;
+	gitService: GitService;
+	fileHandler: FileHandler;
+	commandManager: CommandManager;
+	uiManager: UIManager;
+	private syncInterval: NodeJS.Timer | null = null;
+
+	async onload() {
+		await this.loadSettings();
+
+		this.initializeServices();
+		this.setupViews();
+		this.setupCommands();
+		this.setupEventHandlers();
+		this.setupAutoSync();
+
+		this.addSettingTab(new TeamDocsSettingTab(this.app, this));
+
+		if (this.settings.autoSyncOnStartup) {
+			setTimeout(() => this.gitService.syncTeamDocs(), 2000);
+		}
+
+		setTimeout(() => {
+			this.reservationManager.syncReservationsFromGit();
+		}, 3000);
+
+		console.log("Team Docs Git Sync plugin loaded");
+	}
+
+	onunload() {
+		if (this.syncInterval) {
+			clearInterval(this.syncInterval);
+		}
+		this.reservationManager.getMyReservations().forEach(async (res) => {
+			await this.reservationManager.releaseReservationByPath(res.filePath);
+		});
+		console.log("Team Docs Git Sync plugin unloaded");
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+		this.setupAutoSync();
+	}
+
+	private initializeServices() {
+		this.gitService = new GitService(this.app, this);
+		this.reservationManager = new EditReservationManager(this.app, this);
+		this.statusIndicator = new StatusIndicator(this.app, this);
+		this.fileHandler = new FileHandler(this.app, this);
+		this.commandManager = new CommandManager(this.app, this);
+		this.uiManager = new UIManager(this.app, this);
+
+		this.addChild(this.reservationManager);
+		this.addChild(this.statusIndicator);
+	}
+
+	private setupViews() {
+		this.registerView(
+			ACTIVITY_FEED_VIEW,
+			(leaf) => new TeamActivityFeedView(leaf, this)
+		);
+	}
+
+	private setupCommands() {
+		this.commandManager.registerCommands();
+	}
+
+	private setupEventHandlers() {
+		this.fileHandler.registerEventHandlers();
+	}
+
+	private setupAutoSync() {
+		if (this.syncInterval) {
+			clearInterval(this.syncInterval);
+		}
+
+		if (this.settings.autoSyncInterval > 0) {
+			this.syncInterval = setInterval(
+				() => this.gitService.syncTeamDocs(),
+				this.settings.autoSyncInterval * 60 * 1000
+			);
+		}
+	}
+}
